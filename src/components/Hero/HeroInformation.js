7fad26175f66d9e2048005998ac6d1c4cc6ba295
coder-sys <unix99493@gmail.com>
2022-12-12 16:02:19 +0000

    updated

--- src/components/Hero/HeroInformation.js
+++ src/components/Hero/HeroInformation.js
@@ -8,18 +8,18 @@ const HeroInformation = () => {
 		<HeroSection>
 			<HeroVideo src="./assets/hero.mp4" loop autoPlay muted />
 			<Container>
+				<MainHeading><i>ESPARK</i></MainHeading>
 				<HeroText>
-                ESPARK is a product that belongs to AFD Enterprises' NGO division. It is a web application that allows students and enthusiasts to learn <br></br>
+				ESPARK is a product that belongs to AFD Enterprises' NGO division. It is a web application that allows students and enthusiasts to learn <br></br>
                 concepts faster by providing an adaptive learning platform. This allows students to follow their goals and turn their vision into reality.<br></br>
                 The idea of Espark is based on the curriculum of education in Finland. Espark allows users to store data/websites/videos and podcasts and <br></br>
                 lets students study subjects of their choice.The website is flexible in handling any learning source from the web.The Espark website also <br></br>
                 helps students in creating entrepreneurs because it is built around the user's vision. ESPARK has a vision to help students discover their passion. <br></br>				</HeroText>
-				<Container>
-</Container>
-			</Container>
-			
+				</Container>
+
 		</HeroSection>
 	);
 };
 
 export default HeroInformation;
+
